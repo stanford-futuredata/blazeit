fee4bade85fb6a6d4fb00efbad294284751f2f00
John Taro Guibas <jtguibas@gmail.com>
2020-08-10 19:11:46 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 import setuptools
 
 setuptools.setup(
-    name = "BlazeIt",
-    version = "0.0.1",
-    author = "Daniel Kang",
-    author_email = "ddkang@stanford.edu",
+    name = 'BlazeIt',
+    version = '0.0.1',
+    author = 'Daniel Kang',
+    author_email = 'ddkang@stanford.edu',
     packages=setuptools.find_packages(),
-    python_requires=">=3.5",
+    python_requires='>=3.5',
 )
